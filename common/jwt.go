954ce3cb3a966f881a6d46259b2223a2a58262a9
RK-D <3245885483@qq.com>
2020-05-11 11:28:30 +0800

    only for study

---
+++ common/jwt.go
@@ -0,0 +1,37 @@
+package common
+
+import (
+	"github.com/dgrijalva/jwt-go"
+	"pers.study/cstest/model"
+	"time"
+)
+
+var jwtKey = []byte("a_secret_correct")
+
+type Claims struct {
+	UserId uint
+	jwt.StandardClaims
+}
+
+func ReleaseToken(user model.User) (string, error) {
+	expirationTime := time.Now().Add(3 * 24 * time.Hour)
+	claims := &Claims{
+		UserId: user.ID,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expirationTime.Unix(),
+			IssuedAt:  time.Now().Unix(),
+			Issuer:    "per/RK-D",
+			Subject:   "user token",
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(jwtKey)
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
+}
+
+//token : 协议头(加密协议) +   claims信息 +前面两者加上jwtKey hash的值
+//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VySWQiOjMsImV4cCI6MTU4OTQyNTYyNywiaWF0IjoxNTg5MTY2NDI3LCJpc3MiOiJwZXIvUkstRCIsInN1YiI6InVzZXIgdG9rZW4ifQ.urwn-b5rthHW43FKHO3sX_hpzI6ILAU-a_NWVOP8p1k
+//
--- controller/UserController.go
+++ controller/UserController.go
@@ -2,7 +2,9 @@ package controller
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/gopm/modules/log"
 	"github.com/jinzhu/gorm"
+	"golang.org/x/crypto/bcrypt"
 	"net/http"
 	"pers.study/cstest/common"
 	"pers.study/cstest/model"
@@ -20,7 +22,8 @@ func Register(context *gin.Context) {
 	//2.数据验证
 	//手机
 	if len(telephone) != 11 {
-		context.JSON(http.StatusUnprocessableEntity,
+		context.JSON(
+			http.StatusUnprocessableEntity,
 			// map[string] interface{}
 			//gin源码：type H map[string]interface{}
 			//H is a shortcut for map[string]interface{}
@@ -32,7 +35,8 @@ func Register(context *gin.Context) {
 	}
 	//密码
 	if len(password) < 6 {
-		context.JSON(http.StatusUnprocessableEntity,
+		context.JSON(
+			http.StatusUnprocessableEntity,
 			gin.H{
 				"code": 422,
 				"msg":  "密码不能少于六位",
@@ -46,7 +50,8 @@ func Register(context *gin.Context) {
 
 	//3.手机验证 ,查库
 	if isTelephoneExist(DB, telephone) {
-		context.JSON(http.StatusUnprocessableEntity,
+		context.JSON(
+			http.StatusUnprocessableEntity,
 			gin.H{
 				"code ": 422,
 				"msg":   "用户已存在",
@@ -54,16 +59,29 @@ func Register(context *gin.Context) {
 		return
 	}
 	//不存在创建用户
-	//4.创建用户， 查库
+	//4.创建用户， 查库 查询用户是否重复看手机号是否重复
+	hasPassword, err := bcrypt.GenerateFromPassword([]byte(password),
+		bcrypt.DefaultCost)
+	if err != nil {
+		context.JSON(
+			http.StatusUnprocessableEntity,
+			gin.H{
+				"code ": 500,
+				"msg":   "加密错误",
+			})
+		return
+	}
+
 	newUser := model.User{
 		Name:      name,
 		Telephone: telephone,
-		Password:  password,
+		Password:  string(hasPassword),
 	}
 	DB.Create(&newUser)
 
 	//5.返回结果
-	context.JSON(http.StatusUnprocessableEntity,
+	context.JSON(
+		http.StatusUnprocessableEntity,
 		gin.H{
 			"code ": 200,
 			"msg ":  "注册成功",
@@ -82,10 +100,72 @@ func isTelephoneExist(db *gorm.DB, telephone string) bool {
 }
 
 func Login(context *gin.Context) {
+	DB := common.GetDB()
 	//获取参数
+	telephone := context.PostForm("telephone")
+	password := context.PostForm("password")
 	//数据验证
+	//手机
+	if len(telephone) != 11 {
+		context.JSON(
+			http.StatusUnprocessableEntity,
+			// map[string] interface{}
+			//gin源码：type H map[string]interface{}
+			//H is a shortcut for map[string]interface{}
+			gin.H{
+				"code": 422,
+				"msg":  "手机号必须为11位",
+			})
+		return
+	}
+	//密码
+	if len(password) < 6 {
+		context.JSON(
+			http.StatusUnprocessableEntity,
+			gin.H{
+				"code": 422,
+				"msg":  "密码不能少于六位",
+			})
+		return
+	}
 	//判断手机号
+	var user model.User
+	DB.Where("telephone = ?", telephone).First(&user)
+	if user.ID == 0 {
+		context.JSON(
+			http.StatusUnprocessableEntity,
+			gin.H{
+				"code": 422,
+				"msg":  "用户不存在",
+			})
+	}
 	//判断密码
+
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password)); err != nil {
+		context.JSON(
+			http.StatusUnprocessableEntity, gin.H{
+				"code": 400,
+				"msg":  "密码错误",
+			})
+	}
 	//发放token
+	token, err := common.ReleaseToken(user)
+	if err != nil {
+		context.JSON(
+			http.StatusUnprocessableEntity,
+			gin.H{
+				"code": 500,
+				"msg":  "系统异常",
+			})
+		log.Error("token error : %v", err)
+		return
+	}
 	//返回结果
+	context.JSON(
+		http.StatusUnprocessableEntity,
+		gin.H{
+			"code ": 200,
+			"data ": gin.H{"token ": token},
+			"msg":   "登录成功",
+		})
 }
